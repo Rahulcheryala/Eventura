c219f5c44758d71470848c6d9eb6defa7e9b9582
Rahulcheryala <rahulcheryala2004@gmail.com>
2024-07-20 04:11:59 +0530

    backend setup

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,30 +1,32 @@
 import { connectToDatabase } from "@/lib/database";
-import { auth, currentUser } from "@clerk/nextjs/server";
+// import { auth, currentUser } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-export const GET = async () => {
+export async function GET() {
   try {
-    const { userId } = auth();
-    const user = await currentUser();
+    // const { userId } = auth();
+    // const user = await currentUser();
 
     await connectToDatabase();
 
-    if (!user) {
-      return NextResponse.json(
-        { message: "Not Authenticated" },
-        { status: 404 }
-      );
-    }
+    // if (!user) {
+    //   return NextResponse.json(
+    //     { message: "Not Authenticated" },
+    //     { status: 404 }
+    //   );
+    // }
 
-    return NextResponse.json(
-      {
-        message: "Authenticated",
-        data: { userId: userId, username: user?.username },
-      },
-      { status: 200 }
-    );
+    // return NextResponse.json(
+    //   {
+    //     message: "Authenticated",
+    //     data: { userId: userId, username: user?.username },
+    //   },
+    //   { status: 200 }
+    // );
+
+    return NextResponse.json({ message: "Database connected" });
   } catch (error) {
     console.log(error);
     throw new Error("Error !");
   }
-};
+}
